cd01e436f50b9cf9f220b81e0c1c0052afb226e9
Neznaika76 <n9032692530@gmail.com>
2024-01-18 22:33:42 +0300

    Задание 4

--- Tack8/main.c
+++ Tack8/main.c
@@ -70,13 +70,38 @@ int find_max_array(int size, int a[])
 //  Задание 4
 //
 
-int is_two_same(int size, int a[]);
+int is_two_same(int size, int a[])
 {
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
             for(int j = i + 1; j < size; j++)
                 if (a[i] == a[j])
                     return 1;
     return 0;
+}
+
+//
+//  Задание 5
+//
+
+void change_max_min(int size, int a[])
+{
+    int min = a[0], PosMin=0, max = a[0], PosMax = 0, temp = a[0];
+        for (int i = 1; i < size; i++)
+        {
+            if (min < a[i])
+            {
+                min = a[i];
+                PosMin = i;
+            }
+            if (max > a[i])
+            {
+                max = a[i];
+                PosMax = i;
+            }
+        }
+    temp = a[PosMin];
+    a[PosMin] = a[PosMax];
+    a[PosMax] = temp;
 }
 
 int main(int argc, const char * argv[]) {
